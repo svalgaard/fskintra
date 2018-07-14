a65c996a8f10fcac7481fed15ac0f18f6761f1de
Jens Svalgaard Kohrt <github@svalgaard.net>
2018-07-14 19:23:42 +0200

    Tilføj Python-versionscheck så vi fanger Python 3 i stedet for at give en SyntaxError

--- skoleintra/__init__.py
+++ skoleintra/__init__.py
@@ -1,1 +1,11 @@
 # -*- coding: utf-8 -*-
+
+import sys
+
+#
+# Check that we have Python 2.7.x
+#
+if sys.version_info[0] != 2 or sys.version_info[1] < 7:
+    sys.exit(u'''fskintra kræver Python 2.7.x
+Du kører med version %s
+Måske har du flere Python-versioner installeret?''' % sys.version.split()[0])
